c77358e6265e9f4bad2d7a233ff01b59782f916d
Becomunica <dalayon@becomunica.com>
2023-04-30 21:17:14 +0200

    test configuration

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 
 function App(props) {
 
-  const readGoogleSheet = () => {
-    fetch('https://sheetdb.io/api/v1/gs78hk99h4mox')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-  }
+
 
   return (
     <div>
-      <button onClick={() =>readGoogleSheet()}>Updated</button>
-      <table>
-        <thead>Table name</thead>
-      </table>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
